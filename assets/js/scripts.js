f71aa753d3e2da777974ac697f273fd41e046dc5
Yusfi Syawali <yusfisyawali@gmail.com>
2023-10-02 18:41:57 +0700

    initial commit

---
+++ assets/js/scripts.js
@@ -0,0 +1,7 @@
+function menuOnClick() {
+  const menuBar = document.getElementById("menu-bar");
+  const menu = document.getElementById("menu");
+
+  menuBar.classList.toggle("change");
+  menu.classList.toggle("change");
+}
